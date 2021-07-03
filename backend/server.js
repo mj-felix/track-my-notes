51d15443acfa335bf71bb05cca6dd92cc6d2eb7c
MJ Felix <mjfelixdev@gmail.com>
2021-07-03 20:10:35 +1200

    Fix path for React build folder in Prod

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ connectToDB();
 
 // Serve React app in Prod
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
     app.get('*', (req, res) =>
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     );
