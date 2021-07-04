8b5e8189812fe1602a82f0128cbc3a666c651ff5
MJ Felix <mjfelixdev@gmail.com>
2021-07-05 11:32:50 +1200

    Fix error when user id from JWT is not found in database (post code review)

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -17,6 +17,9 @@ module.exports.verifyAccessToken = asyncHandler(async (req, res, next) => {
             token = req.headers.authorization.split(' ')[1];
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = await User.findById(decoded.content).select('-password');
+            if (!req.user) {
+                throw new Error();
+            }
             next();
         } catch (e) {
             console.log(e.name, e.message, e.expiredAt ? e.expiredAt : '');
