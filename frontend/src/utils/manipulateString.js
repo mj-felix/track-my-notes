8841c1c9bf45a971505120c6b30a64e1052a17df
MJ Felix <mjfelixdev@gmail.com>
2021-07-30 16:19:49 +1200

    Replace regex lookbehind with lookahead due to Safari non-implementation of regex lookbehind

--- frontend/src/utils/manipulateString.js
+++ frontend/src/utils/manipulateString.js
@@ -1,8 +1,8 @@
 export const replaceLinks = (str) => {
-    const pattern = /(?<!")((ftp|http|https|file):\/\/[\S]+(\b|$))/gim;
+    const pattern = /((ftp|https?|file):\/\/[\S]+(\b|$))(?=[\s|<])/gim;
     return str.replace(pattern, "<a href='$1' target='_blank'>$1</a>");
 };
 
-export const breakLines = (str) => str.split(/(?<=a>|\/>|em>|strong>|\b|\n)\n/).join('<br />');
+export const breakLines = (str) => str.split('').reverse().join('').split(/\n(?=>a|>\/|>me|>gnorts|\b|\n)/).join('>/ rb<').split('').reverse().join('');
 
 export const removeProtocol = (url) => url.replace(/(^\w+:|^)\/\//, '');
