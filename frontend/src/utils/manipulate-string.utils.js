f16e38cda68c9e779ee7572a94c140b1fbd39d08
MJ Felix <mjfelixdev@gmail.com>
2021-09-02 16:24:39 +1200

    Fix line breaking

--- frontend/src/utils/manipulate-string.utils.js
+++ frontend/src/utils/manipulate-string.utils.js
@@ -2,7 +2,7 @@ export const replaceLinks = (str) => {
     const pattern = /((ftp|https?|file):\/\/[\S]+(\b|$))(?=[\s|<])/gim;
     return str.replace(pattern, "<a href='$1' target='_blank'>$1</a>");
 };
-
-export const breakLines = (str) => str.split('').reverse().join('').split(/\n(?=>a|>\/|>me|>gnorts|\b|\n|\s|[.!@#$%^&*()_+-=;:,])/).join('>/ rb<').split('').reverse().join('');
+// eslint-disable-next-line
+export const breakLines = (str) => str.split('').reverse().join('').split(/\n(?=>a|>\/|>me|>gnorts|\b|\n|\s|[.?!@#$%^&*()\[\]\{\}_+-=;:,])/).join('>/ rb<').split('').reverse().join('');
 
 export const removeProtocol = (url) => url.replace(/(^\w+:|^)\/\//, '');
