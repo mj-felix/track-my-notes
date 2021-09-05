681eb7e797e4ed9825796703e3647ce8d745835f
MJ Felix <mjfelixdev@gmail.com>
2021-09-05 14:31:53 +1200

    Fix line breaking (ellipsis)

--- frontend/src/utils/manipulate-string.utils.js
+++ frontend/src/utils/manipulate-string.utils.js
@@ -3,6 +3,6 @@ export const replaceLinks = (str) => {
     return str.replace(pattern, "<a href='$1' target='_blank'>$1</a>");
 };
 // eslint-disable-next-line
-export const breakLines = (str) => str.split('').reverse().join('').split(/\n(?=>a|>\/|>me|>gnorts|\b|\n|\s|[.?!@#$%^&*()\[\]\{\}_+-=;:,])/).join('>/ rb<').split('').reverse().join('');
+export const breakLines = (str) => str.split('').reverse().join('').split(/\n(?=>a|>\/|>me|>gnorts|\b|\n|\s|[.?!@#$%^&*()\[\]\{\}_+-=;:,…])/).join('>/ rb<').split('').reverse().join('');
 
 export const removeProtocol = (url) => url.replace(/(^\w+:|^)\/\//, '');
