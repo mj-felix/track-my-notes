d6e7508afdfb93ac803fc8665ec9700d4caebf7c
MJ Felix <mjfelixdev@gmail.com>
2022-01-01 17:15:04 +1300

    Allow u tag in note body

--- backend/middleware/sanitize.middleware.js
+++ backend/middleware/sanitize.middleware.js
@@ -1,11 +1,27 @@
-const sanitizer = require('express-html-sanitizer');
+const sanitizer = require("express-html-sanitizer");
 
 const allowed = {
-    allowedTags: ['em', 'strong', 'a', 'p', 'li', 'ul', 'ol', 'img', 'h1', 'h2', 'h3', 'br', 'hr', 'pre'],
-    allowedAttributes: {
-        'a': ['href', 'target'],
-        'img': ['src', 'alt', 'style'],
-    }
+  allowedTags: [
+    "u",
+    "em",
+    "strong",
+    "a",
+    "p",
+    "li",
+    "ul",
+    "ol",
+    "img",
+    "h1",
+    "h2",
+    "h3",
+    "br",
+    "hr",
+    "pre",
+  ],
+  allowedAttributes: {
+    a: ["href", "target"],
+    img: ["src", "alt", "style"],
+  },
 };
 
 module.exports = sanitizer(allowed);
